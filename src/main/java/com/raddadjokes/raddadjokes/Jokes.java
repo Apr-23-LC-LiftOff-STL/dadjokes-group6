ed182ecbe74a822b7e7cacbddfa916eb532b631a
TaTanisha <tatanisha.wagoner@gmail.com>
2023-05-11 19:51:32 -0500

    new branch

---
+++ src/main/java/com/raddadjokes/raddadjokes/Jokes.java
@@ -0,0 +1,4 @@
+package com.raddadjokes.raddadjokes;
+
+public class Jokes {
+}
---
+++ src/main/java/com/raddadjokes/raddadjokes/User.java
@@ -0,0 +1,7 @@
+package com.raddadjokes.raddadjokes;
+
+import java.util.ArrayList;
+import java.util.List;
+public class User {
+
+}
