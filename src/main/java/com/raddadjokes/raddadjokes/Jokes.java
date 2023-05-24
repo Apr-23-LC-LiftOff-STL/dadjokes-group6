d50c0ddf52d6e5aec79878753a6a818bdce73e34
adriaonline <adriagratiot@gmail.com>
2023-05-24 15:40:03 -0500

    Updated Jokes, Users, application.properties

--- src/main/java/com/raddadjokes/raddadjokes/Jokes.java
+++ src/main/java/com/raddadjokes/raddadjokes/Jokes.java
@@ -4,29 +4,39 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
+import com.raddadjokes.raddadjokes.AbstractEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Table(name = "jokes")
 @Entity
 public class Jokes {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        @Column(nullable = false, updatable = false)
-        private Integer joke_id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Integer joke_id;
 
-        @NotBlank(message = "The joke field cannot be blank.")
-        @Size(min = 10, max = 4000, message = "The joke must be between 10 and 4000 characters.")
-        private String setup;
+    @NotBlank(message = "The joke field cannot be blank.")
+    @Size(min = 10, max = 4000, message = "The joke must be between 10 and 4000 characters.")
+    private String setup;
 
-        @Size(max = 20, message = "Your password must be between 8 and 20 characters.")
-        private String punchline;
+    @Size(max = 20, message = "Your password must be between 8 and 20 characters.")
+    private String punchline;
 
-        private String api_id;
+    private String api_id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private Integer user_id;
 
     public Jokes() {}
 
     public Jokes(String setup, String punchline) {
         this.setup = setup;
         this.punchline = punchline;
+        //return setup + "|" + punchline;
+        //to return use return_value.split("|") gives array of values
     }
 
     public Integer getJoke_id() {
--- src/main/java/com/raddadjokes/raddadjokes/Users.java
+++ src/main/java/com/raddadjokes/raddadjokes/Users.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
+@Table(name = "profiles")
 @Entity
 public class Users {
 
