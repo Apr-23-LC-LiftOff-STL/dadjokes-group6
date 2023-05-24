32c39c956e2ba084e08e80ab05480721f6c91dce
adriaonline <adriagratiot@gmail.com>
2023-05-24 08:59:12 -0500

    Updated Jokes class.

--- src/main/java/com/raddadjokes/raddadjokes/Jokes.java
+++ src/main/java/com/raddadjokes/raddadjokes/Jokes.java
@@ -1,4 +1,76 @@
 package com.raddadjokes.raddadjokes;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
 public class Jokes {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(nullable = false, updatable = false)
+        private Integer joke_id;
+
+        @NotBlank(message = "The joke field cannot be blank.")
+        @Size(min = 10, max = 4000, message = "The joke must be between 10 and 4000 characters.")
+        private String setup;
+
+        @Size(max = 20, message = "Your password must be between 8 and 20 characters.")
+        private String punchline;
+
+        private String api_id;
+
+    public Jokes() {}
+
+    public Jokes(String setup, String punchline) {
+        this.setup = setup;
+        this.punchline = punchline;
+    }
+
+    public Integer getJoke_id() {
+        return joke_id;
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public void setSetup(String setup) {
+        this.setup = setup;
+    }
+
+    public String getPunchline() {
+        return punchline;
+    }
+
+    public void setPunchline(String punchline) {
+        this.punchline = punchline;
+    }
+
+    public String getApi_id() {
+        return api_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Jokes)) return false;
+        Jokes jokes = (Jokes) o;
+        return getJoke_id().equals(jokes.getJoke_id()) && getSetup().equals(jokes.getSetup()) && Objects.equals(getPunchline(), jokes.getPunchline()) && Objects.equals(getApi_id(), jokes.getApi_id());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getJoke_id(), getSetup(), getPunchline(), getApi_id());
+    }
+
+    @Override
+    public String toString() {
+        return "Jokes{" +
+                "setup='" + setup + '\'' +
+                ", punchline='" + punchline + '\'' +
+                '}';
+    }
 }
