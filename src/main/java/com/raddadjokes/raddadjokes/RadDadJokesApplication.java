7afdb178a80e3b40db0174d618771460f06a1b71
mkilp91 <mitchellakilpatrick@gmail.com>
2023-05-29 20:25:59 -0500

    Created development-mitchell branch from mitchell-clean-codebase.

--- src/main/java/com/raddadjokes/raddadjokes/RadDadJokesApplication.java
+++ src/main/java/com/raddadjokes/raddadjokes/RadDadJokesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+//created development-mitchell branch
 
 @SpringBootApplication
 public class RadDadJokesApplication {
