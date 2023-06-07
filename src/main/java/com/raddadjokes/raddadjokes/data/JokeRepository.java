5e4ec9ab3f721a62d5c8a0e07af62c6f2621a5f1
Josh Lin <jlin9695@gmail.com>
2023-06-06 20:39:21 -0500

    Adjusted typings and also changed the User type class.

--- src/main/java/com/raddadjokes/raddadjokes/data/JokeRepository.java
+++ src/main/java/com/raddadjokes/raddadjokes/data/JokeRepository.java
@@ -2,10 +2,20 @@ package com.raddadjokes.raddadjokes.data;
 
 import com.raddadjokes.raddadjokes.models.Joke;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface JokeRepository extends JpaRepository<Joke, Integer> {
+
+    @Query("SELECT j.id FROM Joke j WHERE j.user.id = :userId")
+    Collection<Joke> findJokeIdsByUserId(@Param("userId") Long userId);
+
+    //List<Joke> findAllByUserJokesUserId(Long userId);
 
     Joke findJokeBySetup(String setup);
     Joke findJokeByPunchline(String punchline);
