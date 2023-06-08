84b9947c00a5aee617080c0709759458ecd483a1
adriaonline <adriagratiot@gmail.com>
2023-06-08 12:31:35 -0500

    Taking a break

--- src/main/java/com/raddadjokes/raddadjokes/controllers/MyJokesController.java
+++ src/main/java/com/raddadjokes/raddadjokes/controllers/MyJokesController.java
@@ -11,7 +11,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 import java.util.Collection;
@@ -29,15 +31,12 @@ public class MyJokesController {
 
     @GetMapping
     public String showMyJokes(Model model, Authentication authentication){
-
-
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
         String email = userDetails.getUsername();
         User user = userRepository.findByEmail(email);
         System.out.println(user.getId());
         Integer userId = user.getId();
-
 
 //        Collection<Joke> userJokes = (Collection<Long>) jokeRepository.findJokeIdsByUserId(user.getId());
 //        System.out.println(userJokes);
@@ -63,5 +62,23 @@ public class MyJokesController {
         return ("/my-jokes");
     }
 
+//    6/8 Adria - Copied and modified from CodingEvents with LC Chris, we don't need this bc we already have a page that lists all the jokes
+//    @GetMapping("delete")
+//    public String displayDeleteJokes(Model model) {
+//        model.addAttribute("title", "Delete Joke");
+//        model.addAttribute("events", jokeRepository.findAll());
+//        return "my-jokes/delete";
+//    }
+
+    @PostMapping("delete")
+    public String processDeleteJoke(@RequestParam(required = false) int[] userJokes) {
+
+        if (userJokes != null) {
+            for (int id : userJokes) {
+                jokeRepository.deleteById(id);
+            }
+        }
+        return ("/my-jokes");
+    }
 
 }
